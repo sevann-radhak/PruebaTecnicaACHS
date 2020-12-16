555e0d59eb272a1dd7a3eaad340e2ecf3e31be5a
Sevann Radhak Triztan <sevann.radhak@outlook.com>
2020-12-16 20:57:42 -0300

    Add cors policy

--- PruebaTecnicaACHS/Startup.cs
+++ PruebaTecnicaACHS/Startup.cs
@@ -15,6 +15,8 @@ namespace PruebaTecnicaACHS
 {
     public class Startup
     {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,6 +27,16 @@ namespace PruebaTecnicaACHS
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  builder =>
+                                  {
+                                      builder.WithOrigins("http://localhost:3000")
+                                      .AllowAnyMethod();
+                                  });
+            });
+
             services.AddControllers();
         }
 
@@ -39,6 +51,8 @@ namespace PruebaTecnicaACHS
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseAuthorization();
 
